59999d2198601b1889598536c7e9b37dcc4009a9
y-ueno51 <umrtoon@icloud.com>
2024-07-07 20:09:07 +0900

    Initial Commit

---
+++ Cat_Keyboard/Cat_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_KeyboardApp.swift
+//  Cat_Keyboard
+//
+//  Created by 上野友綺 on 2024/07/07.
+//
+
+import SwiftUI
+
+@main
+struct Cat_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat_Keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cat_Keyboard
+//
+//  Created by 上野友綺 on 2024/07/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
